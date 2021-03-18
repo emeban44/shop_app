19473c1730414f291f48b92f1957f12ff62b516e
emeban44 <emi.cazorla@hotmail.com>
2021-03-18 14:17:37 +0100

    fonts and gridview added

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(13),
+      child: GridTile(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+            color: Colors.redAccent,
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontFamily: 'Lato'),
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
