e8ececc8cec274b28da3e7e78921208a1b038872
emeban44 <emi.cazorla@hotmail.com>
2021-03-18 16:00:40 +0100

    provider for products installed successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        // primaryColor: Colors.lightBlueAccent.shade100,
-        primarySwatch: Colors.blueGrey,
-        accentColor: Colors.lightBlueAccent,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          // primaryColor: Colors.lightBlueAccent.shade100,
+          primarySwatch: Colors.blueGrey,
+          accentColor: Colors.lightBlueAccent,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
