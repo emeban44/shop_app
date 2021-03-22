a2c5ea6732091ad2c8d125134929cafb7b90b2d3
emeban44 <emi.cazorla@hotmail.com>
2021-03-22 13:00:49 +0100

    orders now stored in firebase

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import 'cart.dart';
 
@@ -23,11 +25,29 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url = Uri.https(
+      'first-flutter-backend-default-rtdb.firebaseio.com',
+      '/orders.json',
+    );
+    final timestamp = DateTime.now();
+    final response = await http.post(url,
+        body: json.encode({
+          'amount': total,
+          'dateTime': DateTime.now().toIso8601String(),
+          'products': cartProducts
+              .map((cp) => {
+                    'id': cp.id,
+                    'title': cp.title,
+                    'quantity': cp.quantity,
+                    'price': cp.price,
+                  })
+              .toList(),
+        }));
     _orders.insert(
         0,
         OrderItem(
-          id: DateTime.now().toString(),
+          id: json.decode(response.body)['name'],
           amount: total,
           products: cartProducts,
           dateTime: DateTime.now(),
