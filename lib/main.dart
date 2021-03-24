a28efadd6bd68d910635ac787a1e9b763b76bc5c
emeban44 <emi.cazorla@hotmail.com>
2021-03-24 22:45:01 +0100

    some animations added

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/auth_screen.dart';
+import './helpers/custom_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -52,6 +53,12 @@ class MyApp extends StatelessWidget {
               primarySwatch: Colors.blueGrey,
               accentColor: Colors.lightBlueAccent,
               fontFamily: 'Lato',
+              pageTransitionsTheme: PageTransitionsTheme(
+                builders: {
+                  TargetPlatform.android: CustomPageTransitionBuilder(),
+                  TargetPlatform.iOS: CustomPageTransitionBuilder(),
+                },
+              ),
             ),
             home: auth.isAuth
                 ? ProductsOverviewScreen()
